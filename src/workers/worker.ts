8900586251aa83e189c5616d83cefebcf4ea4477
Renkas Vitalii <ntsn44@gmail.com>
2025-01-27 20:25:45 +0100

    Delete unused dir

--- src/workers/worker.ts
+++
@@ -1,7 +0,0 @@
-import { Agent } from "../core/agent";
-
-self.onmessage = (event) => {
-    const agents = event.data.agents;
-    agents.forEach((agent: Agent) => agent.updateState());
-    self.postMessage({ agents });
-};
